ca3c6a4ceea29b61c1e35b4d5490d1f1aee60775
Kartikee <kartikeemehta4002@gmail.com>
2025-07-11 16:31:26 +0530

    first commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-white bg-[#000]">Hello vite + react</h1>
+    </div>
+  );
+}
+
+export default Home;
