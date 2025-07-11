6ca3d492ff5f2c96f36c909cd933b82f8721bdcb
Kartikee <kartikeemehta4002@gmail.com>
2025-07-11 16:39:39 +0530

    test folder home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Section_a from "./home/Section_a";
 
 function Home() {
   return (
     <div>
       <h1 className="text-white bg-[#000]">Hello vite + react</h1>
+      <Section_a />
     </div>
   );
 }
---
+++ src/pages/home/Section_a.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Section_a() {
+  return <div className="text-yellow">Hello</div>;
+}
+
+export default Section_a;
