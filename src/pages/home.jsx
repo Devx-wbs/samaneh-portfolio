a3b9417555abe38a9489246c9a9b56dc0e3113a3
Nisha <devx@Nishas-MacBook-Air.local>
2025-07-11 17:39:47 +0530

    Crated Home Page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Section_a from "./home/Section_a";
+import Section_b from "./home/Section_b";
 
 function Home() {
   return (
     <div>
       <h1 className="text-white bg-[#000]">Hello vite + react</h1>
       <Section_a />
+      <Section_b/>
     </div>
   );
 }
--- src/pages/home/Section_a.jsx
+++ src/pages/home/Section_a.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-function Section_a() {
-  return <div className="text-yellow">Hello</div>;
-}
+const Section_a = () => {
+  return (
+    <section className="min-h-screen flex items-center bg-[#F5F2EC] px-6 py-16">
+      <div className="max-w-7xl w-full grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
+        <div />
+
+        <div className="text-[#2E2B26]">
+          <h1 className="text-3xl sm:text-4xl md:text-3xl font-bold mb-8">
+            HELLO, I AM KIMIA MOSTADAM
+          </h1>
+          <p className="text-lg sm:text-xl leading-relaxed">
+            I am a UX Researcher using my background in Cognitive Science and Psychology to create meaningful and inclusive digital experiences.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Section_a;
